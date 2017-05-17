f0a720ba5c443a9713896a6adc1d8275e80d100b
Cole Alban <colemalban@gmail.com>
2017-05-17 19:20:26 -0400

    changed folder structure

---
+++ test/test-matchmaking.js
@@ -0,0 +1,29 @@
+/*jshint esversion:6*/
+
+const Matchmaker = require('../src/matchmaking');
+const chai = require('chai');
+const assert = chai.assert;
+
+
+var matchmaker = new Matchmaker();
+describe('Matchmaker', function(){
+    it('Should enqueue a user to the connection queue',function(){
+        assert.fail(null,null ,"Not yet implemented");
+    });
+    it('Should tell if a game is ready to start',function(){
+        assert.fail(null,null,"Not yet implemented");
+    });
+    it('Should clear the queue', function(){
+        assert.fail(null,null,"Not yet implemented");
+    });
+    it('Should remove dead users from the queue',function(){
+        assert.fail(null,null,"Not yet implemented");
+    });
+    it('Should create a new game',function(){
+        assert.fail(null,null,"Not yet implemented");
+    });
+    it('Should return the number of players queued',function(){
+        assert.fail(null,null,"Not yet implemented");
+    });
+
+});
